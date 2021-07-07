d88fc221eff74b6c444fbaa9d652813338e6ba74
FatihHfz <fatihh0021@gmail.com>
2021-07-07 17:01:37 +0300

    encapsulation was done

--- Assets/Scenes/NumberPrediction.cs
+++ Assets/Scenes/NumberPrediction.cs
@@ -10,36 +10,46 @@ public class NumberPrediction : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+        OyunaBasla();
         
-
+    }
+void OyunaBasla()
+{
+        max = 1500;
+        min = 1;
+        tahmin = 750;
         Debug.Log("sayı tahmin oyunu baslıyor....");
         Debug.Log("bir sayı tut");
         Debug.Log("tuttuğun sayı"+ max + "ü gecmesin");
         Debug.Log("tuttuğun sayı"+ min + "den asagı olmasın");
         Debug.Log("tuttuğun sayı"+ tahmin + "mi?");
         Debug.Log("yukarı bas = yükselt, asagı bas = azalt, enter a bas = dogru");
-
-    }
-
+        max +=1;
+}
+void SonrakiTahmin()
+{
+    tahmin = (min + max)/2;
+}
     // Update is called once per frame
     void Update()
     {
-        if(Input.GetKeyDown(KeyCode.UpArrow));
+        if(Input.GetKeyDown(KeyCode.UpArrow))
         {
             min = tahmin;
-            tahmin= (min+max) / 2;
+            SonrakiTahmin();
             Debug.Log("Tahminim:"+ tahmin +"yükseltiyim mi alcaltıyım mı?");
         }
         else if(Input.GetKeyUp(KeyCode.DownArrow))
         {
             max = tahmin;
-            tahmin= (min+max) / 2;
+            SonrakiTahmin();
 
             Debug.Log("Tahminim:"+ tahmin +"yükseltiyim mi alcaltıyım mı?");
         }
         else if(Input.GetKeyDown(KeyCode.Return))
         {
             Debug.Log("ben kazandım");
+            OyunaBasla();
         }
     }
 }
